f86090d24a5502ccb93f955be4b81c545c51c187
robert <robertskeazor@gmail.com>
2018-10-05 15:25:36 -0400

    Add Jetpack dependencies Update Kotlin 1.2.71

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,10 +2,12 @@ object Version {
     val appCompat = "1.0.0"
     val constraintLayout = "2.0.0-alpha2"
     val lifecycle = "2.0.0"
+
     val junit = "4.12"
     val testRunner = "1.1.0-alpha4"
     val espresso = "3.1.0-alpha4"
     val arrow = "0.7.3"
+    val nav = "1.0.0-alpha06"
 
 }
 
@@ -13,10 +15,12 @@ object Deps {
     val appCompat = "androidx.appcompat:appcompat:${Version.appCompat}"
     val constraintLayout = "androidx.constraintlayout:constraintlayout:${Version.constraintLayout}"
     val lifecycle = "androidx.lifecycle:lifecycle-extensions:${Version.lifecycle}"
+    val lifecycle_anonatate = "androidx.lifecycle:lifecycle-compiler:${Version.lifecycle}"
+    val lifecycle_streams = "android.arch.lifecycle:reactivestreams:${Version.lifecycle}"
+    val lifecycle_testing = "android.arch.core:core-testing:${Version.lifecycle}"
     val junit = "junit:junit:${Version.junit}"
     val testRunner = "androidx.test:runner:${Version.testRunner}"
     val espresso = "androidx.test.espresso:espresso-core:${Version.espresso}"
-
     val arrowCore = "io.arrow-kt:arrow-core:${Version.arrow}"
     val arrowTypeClass = "io.arrow-kt:arrow-typeclasses:${Version.arrow}"
     val arrowSyntax = "io.arrow-kt:arrow-syntax:${Version.arrow}"
@@ -24,5 +28,7 @@ object Deps {
     val arrowInstanceCore = "io.arrow-kt:arrow-instances-core:${Version.arrow}"
     val arrowInstanceData = "io.arrow-kt:arrow-instances-data:${Version.arrow}"
     val arrowAnontations = "io.arrow-kt:arrow-annotations-processor:${Version.arrow}"
-
+    val nav_fragment = "android.arch.navigation:navigation-fragment:${Version.nav}"
+    val nav_ui = "android.arch.navigation:navigation-ui:${Version.nav}"
+    val nav_testing = "android.arch.navigation:navigation-testing:${Version.nav}"
 }
